8ba950cab24cdcf04edec70f299afc7575b738f1
lxry-comet <durdentagirowich@gmail.com>
2026-05-06 12:01:38 +0200

    created

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/gsm-cent">
+    <BrowserRouter basename="/gsm-cent-beta">
       <App />
     </BrowserRouter>
   </StrictMode >
